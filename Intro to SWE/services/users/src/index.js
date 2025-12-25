9830d43f1bc2015e6404df4e367431205687ca35
Mohamad Reda <163685910+M0Reda@users.noreply.github.com>
2025-12-26 00:21:45 +0200

    Delete Intro to SWE directory

--- Intro to SWE/services/users/src/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-app.get('/health', (req, res) => res.json({ ok: true }));
-app.get('/users/:id', (req, res) => res.json({ id: req.params.id, name: 'Demo User' }));
-app.listen(3003, () => console.log('users running on 3003'));
